0e29cbf2b17ba73fe465618d6fbf435db889b473
pitagorinpoucak <rookie.1.8@gmail.com>
2022-01-29 15:29:32 +0100

    v0.01 Base Logic - Made random computer player function that returns random item (R/P/S) based on math.random function

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,26 @@
-console.log("Hello TOP");
+function computerPlay() {
+  let randomNumber = Math.floor(Math.random() * 9 + 1);
+  switch (randomNumber) {
+    case 1:
+    case 2:
+    case 3:
+      return "rock";
+    case 4:
+    case 5:
+    case 6:
+      return "paper";
+    case 7:
+    case 8:
+    case 9:
+      return "scissors";
+  }
+}
+
+function gameRound(plSel, compSel) {
+  let playerSelection = plSel.toLowerCase();
+  let computerSelection = compSel.toLowerCase();
+  console.log(playerSelection);
+  console.log(computerSelection);
+}
+
+gameRound("Rock", "pApeR");
