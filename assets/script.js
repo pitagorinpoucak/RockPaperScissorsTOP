57c41edb83a5d2587827baa35af0d9ec13ce15f1
pitagorinpoucak <rookie.1.8@gmail.com>
2022-01-29 16:02:46 +0100

    v0.02 Base game logic - Coded base game logic - Added input checking just in case

--- assets/script.js
+++ assets/script.js
@@ -4,23 +4,66 @@ function computerPlay() {
     case 1:
     case 2:
     case 3:
-      return "rock";
+      return "Rock";
     case 4:
     case 5:
     case 6:
-      return "paper";
+      return "Paper";
     case 7:
     case 8:
     case 9:
-      return "scissors";
+      return "Scissors";
   }
 }
 
-function gameRound(plSel, compSel) {
+function playRound(plSel, compSel) {
   let playerSelection = plSel.toLowerCase();
   let computerSelection = compSel.toLowerCase();
-  console.log(playerSelection);
-  console.log(computerSelection);
+
+  if (playerSelection === computerSelection) {
+    return `Both players chose ${compSel}! It's a TIE!`;
+  }
+  switch (playerSelection) {
+    case "rock":
+      if (computerSelection === "paper") {
+        return "You LOSE! Paper beats Rock.";
+      } else {
+        return "You WIN! Rock beats Scissors.";
+      }
+    case "paper":
+      if (computerSelection === "scissors") {
+        return "You LOSE! Scissors cuts Paper.";
+      } else {
+        return "You WIN! Paper covers Rock.";
+      }
+    case "scissors":
+      if (computerSelection === "rock") {
+        return "You LOSE! Rock crushes Scissors.";
+      } else {
+        return "You WIN! Scissors cut Paper.";
+      }
+    default:
+  }
 }
 
-gameRound("Rock", "pApeR");
+function checkInput(input) {
+  let bool = true;
+  inputLower = input.toLowerCase();
+
+  while (bool) {
+    if (
+      inputLower === "rock" ||
+      inputLower === "paper" ||
+      inputLower === "scissors"
+    ) {
+      return inputLower;
+    } else {
+      inputLower = prompt(
+        "Please input correct expression: Rock/Paper/Scissors. Case doesn't matter"
+      ).toLowerCase();
+    }
+  }
+}
+
+let playerInput = prompt("Rock/Paper/Scissors?");
+console.log(playRound(checkInput(playerInput), computerPlay()));
